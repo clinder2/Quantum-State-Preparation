83dfb2bd24e4ea206d0a50ec9eb801708f3cb913
joel-koshy <joeljeny@gmail.com>
2025-09-30 14:33:52 -0400

    basic builder setup

---
+++ AnsatzPruning/AnsatzBenchmarking/Builders/fixedSU2.py
@@ -0,0 +1,10 @@
+from .base import AnsatzBuilder 
+from qiskit.circuit.library import EfficientSU2
+
+class FixedSU2Builder(AnsatzBuilder):
+    # Ansatz builder with SU2 
+
+    def build(self):
+        n_qubits = self.hamiltonian.num_qubits
+        self.circuit = EfficientSU2(n_qubits, reps=2)
+        return self.circuit
